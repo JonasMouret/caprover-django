30c6295af0b34ab8292b11f39f53aa563c46630e
jonas.mouret <jonas@Jonas-PC.localdomain>
2020-11-27 17:32:12 +0100

    pattttsddsds

--- utils/run.sh
+++ utils/run.sh
@@ -2,6 +2,6 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
-# gunicorn imageAPI.wsgi --bind=0.0.0.0:80
+# python manage.py runserver 0.0.0.0:8000
+gunicorn imageAPI.wsgi --bind=0.0.0.0:80
 
