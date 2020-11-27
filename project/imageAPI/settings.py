fb32fee983719312642398ee4fd3fbec536d77bb
jonas.mouret <jonas@Jonas-PC.localdomain>
2020-11-27 18:50:30 +0100

    change run ch

--- project/imageAPI/settings.py
+++ project/imageAPI/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     "rest_framework.authtoken",
-    'API',
+    # 'API',
     'corsheaders',
     "API.apps.BelierApiConfig",
 ]
